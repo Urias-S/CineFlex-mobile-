823c2e1a8ab572ebbe7c7d44aabc4b2f68da1d10
Vinicius <viniciusmoura761@gmail.com>
2025-03-25 19:32:27 -0300

    feat: add on display movies section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import GlobalStyle from "./style/GlobalStyle"
 import { BrowserRouter, Routes, Route } from "react-router"
 import Header from '../src/components/Header'
-import styled from "styled-components"
+import OnDisplay from "./components/OnDisplay"
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+
+
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+      axios.get('https://mock-api.driven.com.br/api/v8/cineflex/movies')
+      .then(res => setMovies(res.data))
+      .catch(error => error.data)
+  }, [])
+
   return (
-      <BrowserRouter>
-        <GlobalStyle />
-        <Header />
-      </BrowserRouter>
+    <BrowserRouter>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route path="/" element={<OnDisplay movies = {movies}/>}></Route>
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 export default App
--- src/components/OnDisplay.jsx
+++ src/components/OnDisplay.jsx
@@ -1,5 +1,58 @@
-export default function OnDisplay () {
+import styled from "styled-components";
+
+export default function OnDisplay({ movies }) {
   return (
-    <>OnDisplay</>
+    <>
+      <Display>
+        <Title>
+          <TitleName>Em Cartaz</TitleName>
+        </Title>
+        <MoviesSection>
+          {movies.map((movie) => {
+            return (<Poster src={movie.posterURL} key={movie.id} />);
+          })}
+        </MoviesSection>
+      </Display>
+    </>
   );
 }
+
+const MoviesSection = styled.div`
+  width: 90vw;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 25px;
+  padding-bottom: 25px;
+`;
+
+const Poster = styled.img`
+  width: 145px;
+  height: auto;
+  border-radius: 8px;
+`;
+
+const Title = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 25px 0;
+`;
+
+const TitleName = styled.p`
+    font-family: "Sarala", sans-serif;
+    font-size: 30px;
+    color: white;
+`;
+
+const Display = styled.div`
+  @media (max-width: 768px) {
+    background-color: rgba(33, 34, 38, 1);
+    width: 100vw;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+`;
